3c0e49a890f5d29c082b9995ba57a46306d681eb
MChammer0550 <mchammer0550@gmail.com>
2025-10-24 12:35:11 +0300

    v3.0.0
    версия v3.0.0 т.к. был добавлен совершенно новый функционал

--- test.py
+++ test.py
@@ -1,2 +1,18 @@
+import time
 user_input = input("Введите что будет выведено: ")
+
+# экспериментальный и ОЧЕНЬ полезный функционал
+time.sleep(1.5)
+print("обработка ввода.")
+
+time.sleep(1)
+print("обработка ввода..")
+
+time.sleep(1)
+print("обработка ввода...")
+
+time.sleep(2)
+print("почти готово")
+
+time.sleep(3)
 print(user_input)
